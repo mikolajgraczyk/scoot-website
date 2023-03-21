fb5311b6e4874a878d4487581131cef5e6baf82a
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-03-21 15:47:50 +0100

    added theme and global style files as well as necessary fonts

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    color: {
+        white: "#FFFFFF",
+        yellow: "#FCB72B",
+        darkNavy: "#495567",
+        dimGrey: "#939CAA",
+        lightGrey: "#E5ECF4",
+        snow: "#F2F5F9",
+        lightYellow: "#FFF4DF",
+    },
+    breakpoint: {},
+};
