edb767f73913cd9311924eb5d4b4f230ac8dd895
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-04-10 23:01:59 +0200

    adjusted instruction element for mobile screens

--- src/HomePage/Instruction/InstructionElement/styled.js
+++ src/HomePage/Instruction/InstructionElement/styled.js
@@ -11,14 +11,28 @@ export const StyledInstructionElement = styled.div`
     flex-direction: row;
     align-items: top;
     gap: 70px;
-  } ;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    max-width: 311px;
+    flex-direction: column;
+    gap: 24px;
+    align-items: center;
+  }
 `;
 
 export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 27px;
+
   @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
-    flex-direction: row;
     max-width: 398px;
-  } ;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    gap: 24px;
+  }
 `;
 
 export const Title = styled.div`
@@ -28,12 +42,21 @@ export const Title = styled.div`
   line-height: 28px;
   letter-spacing: -1.07143px;
   color: ${({ theme }) => theme.color.darkNavy};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    text-align: center;
+    font-size: 20px;
+    letter-spacing: -0.892857px;
+  }
 `;
 
 export const Description = styled.div`
-  margin-top: 27px;
   font-family: "Lexend Deca";
   font-size: 15px;
   line-height: 25px;
   color: ${({ theme }) => theme.color.dimGrey};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    text-align: center;
+  }
 `;
--- src/HomePage/Instruction/styled.js
+++ src/HomePage/Instruction/styled.js
@@ -5,22 +5,26 @@ export const StyledInstruction = styled.div`
 
   @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
     margin-top: 0;
-  } ;
+  }
 
-  @media(max-width: ${({theme}) => theme.breakpoint.mobile}px){
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     margin-top: 0;
-  };
+  } ;
 `;
 
 export const Wrapper = styled.div`
-    display: flex;
-    gap: 30px;
-    margin-left: 11.5%;
+  display: flex;
+  gap: 30px;
+  margin-left: 11.5%;
 
-    @media(max-width: ${({theme}) => theme.breakpoint.tablet}px){
-        flex-direction: column;
-        align-items: center;
-        margin-left: 0;
-        margin-top: 122px;
-    };
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
+    flex-direction: column;
+    align-items: center;
+    margin-left: 0;
+    margin-top: 122px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    margin-top: 120px;
+  }
 `;
