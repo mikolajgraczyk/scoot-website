4a9a6395d02871097a459adc510a0a457b4806a1
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-04-12 13:16:04 +0200

    added new About component

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -27,6 +27,13 @@ const HomePage = () => {
             "Scoot is available in 4 major cities so far. We’re expanding rapidly, so be sure to let us know if you want to see us in your hometown. We’re aiming to let our scooters loose on 23 cities over the coming year."
           }
         />
+        <About
+          image={payments}
+          title={"Zero hassle payments"}
+          description={
+            "Our payment is as easy as one two three. We accept most credit cards and debit cards. You can also link your PayPal account inside the app. Need to pay later? No worries! You can defer payment for up to a month."
+          }
+        />
       </AboutSection>
     </StyledHomepage>
   );
