9db7a11b58cc7a5879553c075544f371d38ebf4d
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-06-29 18:15:10 +0200

    added info element to careers subpage

--- src/CareersPage/index.js
+++ src/CareersPage/index.js
@@ -1,12 +1,30 @@
 import { StyledCareersPage } from "./styled";
 import TopBanner from "../common/TopBanner";
+import Info from "../common/Info";
+import { InfoSection } from "../common/InfoSection";
+import { Container } from "../common/Container";
+import joinus from "./images/join-us.jpg";
 
 const CareersPage = () => {
-    return(
-        <StyledCareersPage>
-            <TopBanner title="Careers" location={"careers"}/>
-        </StyledCareersPage>
-    );
+  const location = "subpage";
+
+  return (
+    <StyledCareersPage>
+      <TopBanner title="Careers" location={"careers"} />
+      <Container>
+        <InfoSection location={location}>
+          <Info
+            image={joinus}
+            title={"Care to join our mission?"}
+            description={
+              "We’re always looking for ambitious individuals to help us on our journey. If you’re passionate about our mission to provide clean, accessible transport to improve urban living we want to hear from you!"
+            }
+            location={"subpage"}
+          />
+        </InfoSection>
+      </Container>
+    </StyledCareersPage>
+  );
 };
 
 export default CareersPage;
--- src/CareersPage/styled.js
+++ src/CareersPage/styled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
 export const StyledCareersPage = styled.main`
-
+  overflow: hidden;
 `;
